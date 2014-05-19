982328cdbebe250839a72982fa00dd85c8021eb9
solomonsuheng <1185626206@qq.com>
2014-05-19 11:18:22 +0800

    最简单的HttpClient Get访问方式

---
+++ HttpConn/Get/EasyHttpGet.java
@@ -0,0 +1,27 @@
+package GetAndPost;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Request;
+
+/**
+ * @ClassName: EasyHttpGet
+ * @Description: TODO最简单的HttpGets
+ * @author solomonsuheng
+ * @date 2014年5月19日 上午11:14:30
+ */
+public class EasyHttpGet {
+	public static void main(String[] args) {
+		try {
+			System.out.println(Request.Get("http://www.google.com.hk")
+					.execute().returnContent());
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
